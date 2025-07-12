1e1219ab716082eaf2ca43457464301f154ee21b
XBigTK13X <xbigtk13x@gmail.com>
2025-07-12 14:15:14 -0500

    Correct APK name

--- script/full-release.sh
+++ script/full-release.sh
@@ -2,17 +2,11 @@
 
 export NODE_ENV="production"
 
-echo "=-=- Building the container image -=-="
-script/docker-build.sh push
-
-echo "=-=- Running the latest version container on beast -=-="
-ssh access@beast.9914.us "bash -c \"cd /mnt/docker; ./on/snowstream.sh\""
-
 echo "=-=- Build the apks -=-="
 script/prod-generate-apks.sh
 
 echo "=-=- Push the apks up to the file server -=-="
-~/script/push-apks.sh snowstream
+~/script/push-apks.sh snowpage
 
 echo "=-=- Deploy the apks to all devices -=-="
 ~/script/remote-adb.py
--- script/prod-generate-apks.sh
+++ script/prod-generate-apks.sh
@@ -4,11 +4,6 @@ echo "THIS IS THE ONE TO USE!"
 
 cd expo/android
 mkdir -p build-out
-export EXPO_TV=0
-./gradlew assembleRelease
-#./gradlew release
-cp app/build/outputs/apk/release/app-release.apk build-out/snowstream-mobile.apk
 export EXPO_TV=1
 ./gradlew assembleRelease
-#./gradlew release
-cp app/build/outputs/apk/release/app-release.apk build-out/snowstream-tv.apk
+cp app/build/outputs/apk/release/app-release.apk build-out/snowpage-tv.apk
