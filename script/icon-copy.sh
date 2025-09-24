cf2b9d82f4e9903d54bde9cd29275dc9d5878683
XBigTK13X <xbigtk13x@gmail.com>
2025-09-24 12:29:27 -0500

    Bring over the updated app icons

---
+++ script/icon-copy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+mkdir -p ./expo/assets/icon/android
+
+cp ~/develop/snowappicons/generated/snowpage/tvbanner.png expo/assets/icon/android/tvbanner.png
+
+cp ~/develop/snowappicons/generated/snowpage/appicon.png expo/assets/icon/appicon.png
+
+cp ~/develop/snowappicons/generated/snowpage/splash.png expo/assets/icon/splash.png
