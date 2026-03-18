76c03af691e7ba66a471b8f564c6e2f300e13b1c
XBigTK13X <xbigtk13x@gmail.com>
2026-03-18 16:13:39 -0500

    Fix the deployment script

--- script/full-release.sh
+++ script/full-release.sh
@@ -8,7 +8,7 @@ echo "=-=- Build the apks -=-="
 script/prod-generate-apks.sh
 
 echo "=-=- Push the apks up to the file server -=-="
-~/script/push-apks.sh snowpage
+~/script/push-apks.py snowpage
 
 echo "=-=- Deploy the apks to all devices -=-="
 ~/script/remote-adb.py All deploy_snowpage
